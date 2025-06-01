6793275bf75e6e1170241c2baee0dcee0f6108f4
zuam <apkssky@gmail.com>
2025-06-01 09:17:29 +0700

    fix issue

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\API\PatientController;
 use Illuminate\Support\Facades\Route;
 
+const PATIENTBYID = '/patients/{id}';
+
 Route::middleware(['check.apikey'])
     ->name('api.')
     ->group(function () {
         Route::post('/patients', [PatientController::class, 'create'])->name('patients.create');
-        Route::put('/patients/{id}', [PatientController::class, 'update'])->name('patients.update');
-        Route::delete('/patients/{id}', [PatientController::class, 'delete'])->name('patients.delete');
+        Route::put(PATIENTBYID, [PatientController::class, 'update'])->name('patients.update');
+        Route::delete(PATIENTBYID, [PatientController::class, 'delete'])->name('patients.delete');
         Route::get('/patients', [PatientController::class, 'get'])->name('patients.get');
-        Route::get('/patients/{id}', [PatientController::class, 'getById'])->name('patients.getById');
+        Route::get(PATIENTBYID, [PatientController::class, 'getById'])->name('patients.getById');
     });
